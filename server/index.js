4b44a2e055fccfce8fce5783a636b0ca3588b39b
Muhammad Hassan <hassanshakil12@gmail.com>
2024-10-06 06:07:27 +0500

    setup logger

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
+import winston from "winston";
 import app from "./app.js";
 import config from "./config/env.config.js";
 
+const logger = winston.createLogger({
+  level: config.logLevel,
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
 const startServer = () => {
   app.listen(config.port, (err) => {
-    if (err) throw new Error();
-    console.log(`Server is listening on port ${config.port} in ${config.nodeEnv} mode`);
+    logger.error(
+      `Server is listening on port ${config.port} in ${config.nodeEnv} mode`
+    );
   });
 };
 
-startServer()
+startServer();
