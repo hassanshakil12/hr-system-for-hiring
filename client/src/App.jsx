14294e790a3e4a007881c92592725d605b436835
Muhammad Hassan <hassanshakil12@gmail.com>
2024-10-26 07:22:42 +0500

    Added Login and Register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Home, Profile } from "./pages";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { Home, Profile, Login, Register } from "./pages";
+import { Navbar } from "./components";
 
 const App = () => {
   return (
     <>
+      <Router>
+        <NavigationRoutes />
+      </Router>
+    </>
+  );
+};
+
+const NavigationRoutes = () => {
+  const location = useLocation();
+  const noNavigationPaths = ["/login", "/register"];
+  return (
+    <>
+      {!noNavigationPaths.includes(location.pathname) && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,7 +4,6 @@ import { Navbar } from "../components";
 const Profile = () => {
   return (
     <>
-      <Navbar />
       <div>Profile</div>
     </>
   );
