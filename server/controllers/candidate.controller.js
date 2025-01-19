1691e24b8d2e5ea654790bb9136e783c3cff73c7
Muhammad Hassan <hassanshakil12@gmail.com>
2025-01-19 06:32:15 +0500

    fetching job by searching for params

--- server/controllers/candidate.controller.js
+++ server/controllers/candidate.controller.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import Job from "../models/job.model.js";
 import JobApplication from "../models/jobApplication.model.js";
 
@@ -21,13 +22,44 @@ export const getJobs = async (req, res) => {
   }
 };
 
+export const getJobById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const job = await Job.findOne({ _id: id }).populate(
+      "organization",
+      "username email"
+    );
+    if (!job) {
+      return res.status(404).json({ message: "Job not found", success: false });
+    }
+
+    res.status(200).json({ job, success: true });
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+};
+
 export const applyForJob = async (req, res) => {
   const { job, coverLetter } = req.body;
 
   try {
     const jobExists = await Job.findById(job);
     if (!jobExists) {
+      if (req.file) fs.unlinkSync(req.file.path);
       return res.status(404).json({ message: "Job not found", success: false });
+    }
+
+    const existingApplication = await JobApplication.findOne().where({
+      job,
+      candidate: req.user._id,
+    });
+
+    if (existingApplication) {
+      if (req.file) fs.unlinkSync(req.file.path);
+      return res.status(400).json({
+        message: "You have already applied to this job",
+        success: false,
+      });
     }
 
     if (!req.file) {
@@ -45,12 +77,13 @@ export const applyForJob = async (req, res) => {
 
     await jobApplication.save();
 
-    res.status(201).json({
+    return res.status(201).json({
       message: "Application submitted successfully",
       success: true,
       application: jobApplication,
     });
   } catch (error) {
+    if (req.file) fs.unlinkSync(req.file.path);
     console.error("Error applying to job:", error);
     res.status(500).json({ message: "Internal server error", success: false });
   }
--- server/routes/candidate.routes.js
+++ server/routes/candidate.routes.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { tokenAuthentication } from "../middlewares/auth.js";
-import { getJobs, applyForJob } from "../controllers/candidate.controller.js";
+import {
+  getJobs,
+  applyForJob,
+  getJobById,
+} from "../controllers/candidate.controller.js";
 import upload from "../middlewares/multer.js";
 
 const router = express.Router();
 
 router.get("/", tokenAuthentication, getJobs);
+router.get("/job/:id", tokenAuthentication, getJobById);
 router.post(
   "/apply-for-job",
   tokenAuthentication,
