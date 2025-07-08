bd00efd81676b697e129f5cfd8f51ce57bb7bdc9
Muhammad Hassan <hassanshakil12@gmail.com>
2025-07-09 00:15:12 +0500

    Deleted: Server and Client folder

--- client/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from "./Navbar.jsx";
-export { default as SearchBar } from "./SearchBar.jsx";
-export { default as ProfileCard } from "./ProfileCard.jsx";
-export { default as SignOutBtn } from "./SignOutBtn.jsx";
-export { default as ProtectedRoutes } from "./ProtectedRoutes.jsx";
-export { default as RestrictedRoutes } from "./RestrictedRoutes.jsx";
-export { default as JobCard } from "./JobCard.jsx";
--- server/middlewares/organizationAuthorization.js
+++
@@ -1,6 +0,0 @@
-export const organizationAuthorization = async (req, res, next) => {
-    if(req.user.entityType !== "organization") {
-        return res.status(403).json({ message: "Unauthorized: Access denied", success: false });
-    }
-    next();
-}
